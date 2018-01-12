5d4ab23ddc3a8232987109e0a85316e404e62e46
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-01-12 16:04:31 -0600

    assigned and display p1 vs p2 for battle

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,114 +1,147 @@
 $(document).ready(function(){
-  var config = {
-    apiKey: "AIzaSyA7ta-TS3GcFmLzxhl1SwZuEVIqDbzmHfc",
-    authDomain: "rps-qg.firebaseapp.com",
-    databaseURL: "https://rps-qg.firebaseio.com",
-    projectId: "rps-qg",
-    storageBucket: "rps-qg.appspot.com",
-    messagingSenderId: "946643017034"
-  };
-  firebase.initializeApp(config);
+    var config = {
+      apiKey: "AIzaSyA7ta-TS3GcFmLzxhl1SwZuEVIqDbzmHfc",
+      authDomain: "rps-qg.firebaseapp.com",
+      databaseURL: "https://rps-qg.firebaseio.com",
+      projectId: "rps-qg",
+      storageBucket: "rps-qg.appspot.com",
+      messagingSenderId: "946643017034"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+    var playersRef = database.ref("/playerData");
+  //monitor how many people are on the RPS sites.
+    var connectedRef = database.ref(".info/connected");
+    var connectionsRef = database.ref("/connections");
+    connectedRef.on("value",function(snap){
+      console.log(snap.val());
+      if(snap.val()){
+        var con = connectionsRef.push(true);
+        console.log("user connected");
+        con.onDisconnect().remove();
+        database.ref("/playerData/p1").onDisconnect().remove();
+        database.ref("/playerData/p2").onDisconnect().remove();    
+      }   
+    })
+    var numConnections;
+    connectionsRef.on("value",function(snap){
+      numConnections = snap.numChildren();
+      console.log("numConnections" + numConnections);
+    })
 
-  var database = firebase.database();
-  var player1Name;
-  var player1Wins;
-  var player1Losses;
-  var player2Name;
-  var player2Wins;
-  var player2Losses;
+    function displayOptions(){
+        $("."+userRole).find(".rock").text("Rock");
+        $("."+userRole).find(".paper").text("Paper");
+        $("."+userRole).find(".scissors").text("Scissors");
+        $("."+opRole).find(".option").hide();
+    };
 
-  var playersRef = database.ref("/playerData");
-  var player1Ref = database.ref("/playerData/1");
-  var player2Ref = database.ref("/playerData/2"); 
-  var turnsRef = database.ref("/turns"); 
+    var userWins;
+    var userLosses;
+    var opName;
+    var opWins;
+    var opLosses;
+    var userName;
+    var userRole;
+    var opRole;
+    var numPlayers;
+    var playerlist;
 
-  var playerSignInRef = database.ref("/signIn"); 
+    
+    var userRef = database.ref("/playerData/"+userRole);
+    var opRef = database.ref("/playerData/"+opRole);
+  
+    
+    database.ref("/playerData/p1").on("value",function(snap){
+        var p1_name; 
+        var p1_Wins; 
+        var p1_Losses;   
+        if(snap.val()){
+            p1_name = snap.val().name;
+            p1_Wins = snap.val().wins;
+            p1_Losses = snap.val().losses;
+        } else {
+            p1_name = "Waiting for player 1";
+            p1_Wins = 0;
+            p1_Losses = 0;
+        }
+        $(".p1").find("h3").text(p1_name);
+        $(".p1").find(".wins").text("Wins: "+ p1_Wins);
+        $(".p1").find(".losses").text("Losses: "+ p1_Losses);
+    })  
 
-  player1Ref.on("value",function(snap){
-    if(snap.child("name").exists()){
-      player1Name = snap.val().name;
-      player1Wins = snap.val().wins;
-      player1Losses = snap.val().losses;
-      $(".player1").find("h3").text(player1Name);
-      $(".player1").find(".wins").text("Wins: "+player1Wins);
-      $(".player1").find(".losses").text("Losses: "+player1Losses);
-    } else { 
-      $(".player1").find("h3").text("Waiting for Player 1");
-    }
-  })
-
-  player2Ref.on("value",function(snap){
-    if(snap.child("name").exists()){
-      player2Name = snap.val().name;
-      player2Wins = snap.val().wins;
-      player2Losses = snap.val().losses;
-      $(".player2").find("h3").text(player2Name);
-      $(".player2").find(".wins").text("Wins: "+player2Wins);
-      $(".player2").find(".losses").text("Losses: "+player2Losses); 
-    } else {
-      $(".player2").find("h3").text("Waiting for Player 2");
-    }
-  })
-
-  var player1Exist;
-  var player2Exist;
-  $(".signInButton").on("click",function(){
-    event.preventDefault();
-    var userName = $(".name").val();
-    if(!player1Name){
-      player1Ref.set({
-        name: userName,
-        wins:0,
-        losses:0,
-      });
-    } else if(!player2Name){
-      player2Ref.set({
-        name: userName,
-        wins:0,
-        losses:0
-      });
-    } else {
-      console.log("already has enough players!")
-    }
-  })
-
-
-  var player1Choice;
-  var player2Choice;
-  var numPlayers = 0;
-  playersRef.on("child_added",function(snap){
-    numPlayers++; 
-    if(numPlayers === 2){
-      displayOptions(1);
-      displayOptions(2);
-    }
-  }) 
-
-  function displayOptions(id){
-      var rock = $("<p>").text("Rock")
-      var paper = $("<p>").text("Paper")
-      var scissors = $("<p>").text("Scissors")
-      $(".player"+id).append(rock).append(paper).append(scissors);
-  }
+    database.ref("/playerData/p2").on("value",function(snap){
+        var p2_name; 
+        var p2_Wins; 
+        var p2_Losses;   
+        if(snap.val()){
+            p2_name = snap.val().name;
+            p2_Wins = snap.val().wins;
+            p2_Losses = snap.val().losses;
+        } else {
+            p2_name = "Waiting for player 2";
+            p2_Wins = 0;
+            p2_Losses = 0;
+        }
+        $(".p2").find("h3").text(p2_name);
+        $(".p2").find(".wins").text("Wins: "+ p2_Wins);
+        $(".p2").find(".losses").text("Losses: "+ p2_Losses);
+    })
 
 
+    $("."+userRole).on("click",".option",function(){
+        var choice = $(this).attr("data-choice");
 
-//monitor how many people are on the RPS sites.
-  var connectedRef = database.ref(".info/connected");
-  var connectionsRef = database.ref("/connections");
-  connectedRef.on("value",function(snap){
-    console.log(snap.val());
-    if(snap.val()){
-      var con = connectionsRef.push(true);
-      console.log("user connected");
-      con.onDisconnect().remove()
-    }   
-  })
-  var numConnections;
-  connectionsRef.on("value",function(snap){
-    numConnections = snap.numChildren();
-    console.log(numConnections);
-  })
+    })
+
+    playersRef.on("value",function(snap){
+        numPlayers = snap.numChildren();
+        if(snap.val()){
+            playerlist = Object.keys(snap.val());
+            console.log(playerlist);
+        };
+        console.log("np"+numPlayers)
+    })
+
+    playersRef.on("child_added",function(snap,prevChildkey){
+        if(prevChildkey){
+            displayOptions();
+            console.log("both ready")
+        }
+    })
+    
+
  
+    $(".signInButton").on("click",function(){
+        event.preventDefault();
+        userName = $(".name").val();
+        if(numPlayers === 2){
+            console.log("too many");
+        } else if(numPlayers===0){
+            userRole = "p1";
+            opRole = "p2";
+            playersRef.child(userRole).set({
+                name: userName,
+                wins:0,
+                losses:0,
+            }); 
+        $(".signIn").hide();
+        $(".note").html(`<h3>hi ${userName}, you are player ${userRole[1]}</h3>`)    
+        } else if(numPlayers===1){
+            userRole = playerlist[0] === "p1" ? "p2":"p1";
+            opRole = playerlist[0];
+            playersRef.child(userRole).set({
+                name: userName,
+                wins:0,
+                losses:0,
+            });
+            $(".signIn").hide();
+            $(".note").html(`<h3>hi ${userName}, you are player ${userRole[1]}</h3>`)   
+        }
+        console.log(userRole,opRole); 
+    });
 
-}) 
+
+  
+  }) 
+  
