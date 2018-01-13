d574b56a2484f6e316f196072abecf2d5afea800
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-01-13 10:02:42 -0600

    set for testing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,8 +19,6 @@ $(document).ready(function(){
         var con = connectionsRef.push(true);
         console.log("user connected");
         con.onDisconnect().remove();
-        // database.ref("/playerData/p1").onDisconnect().remove();
-        // database.ref("/playerData/p2").onDisconnect().remove();    
       }   
     })
     var numConnections;
@@ -51,54 +49,66 @@ $(document).ready(function(){
     var p1Ref = database.ref("/playerData/p1");
     var p2Ref = database.ref("/playerData/p2");
   
-    
+    var p1_name; 
+    var p1_Wins; 
+    var p1_Losses;
+    var p1_choice;   
     p1Ref.on("value",function(snap){
-        var p1_name; 
-        var p1_Wins; 
-        var p1_Losses;   
         if(snap.val()){
             p1_name = snap.val().name;
             p1_Wins = snap.val().wins;
             p1_Losses = snap.val().losses;
+            $(".p1").find(".wins").text("Wins: "+ p1_Wins);
+            $(".p1").find(".losses").text("Losses: "+ p1_Losses);
+            if(snap.val().choice){
+                p1_choice = snap.val().choice;
+            }
         } else {
             p1_name = "Waiting for player 1";
             p1_Wins = 0;
             p1_Losses = 0;
         }
         $(".p1").find("h3").text(p1_name);
-        $(".p1").find(".wins").text("Wins: "+ p1_Wins);
-        $(".p1").find(".losses").text("Losses: "+ p1_Losses);
+        console.log("p1chocie"+p1_choice)
+
     })  
 
+    var p2_name; 
+    var p2_Wins; 
+    var p2_Losses;
+    var p2_choice;   
     p2Ref.on("value",function(snap){
-        var p2_name; 
-        var p2_Wins; 
-        var p2_Losses;   
         if(snap.val()){
             p2_name = snap.val().name;
             p2_Wins = snap.val().wins;
             p2_Losses = snap.val().losses;
+            $(".p2").find(".wins").text("Wins: "+ p2_Wins);
+            $(".p2").find(".losses").text("Losses: "+ p2_Losses);
+            if(snap.val().choice){
+                p2_choice = snap.val().choice;
+            }
         } else {
             p2_name = "Waiting for player 2";
             p2_Wins = 0;
             p2_Losses = 0;
         }
         $(".p2").find("h3").text(p2_name);
-        $(".p2").find(".wins").text("Wins: "+ p2_Wins);
-        $(".p2").find(".losses").text("Losses: "+ p2_Losses);
+        console.log("p2choice"+p2_choice)
+    
     })
 
     $(".p1").on("click",".option",function(){
-        var p1_choice = $(this).attr("data-choice");
-        p1Ref.child("choice").set(p1_choice)
+        var choice = $(this).attr("data-choice");
+        console.log(choice)
+        p1Ref.child("choice").set(choice)
         database.ref().child("turn").set(2);        
     })
 
     $(".p2").on("click",".option",function(){
-        var p2_choice = $(this).attr("data-choice");
-        p2Ref.child("choice").set(p2_choice)
-        database.ref().child("turn").set(1);
-     
+        var choice = $(this).attr("data-choice");
+        p2Ref.child("choice").set(choice, getResult)
+        database.ref().child("turn").set(3);
+        // getResult();
     })
 
     database.ref().child("turn").on("value",function(snap){
@@ -116,11 +126,11 @@ $(document).ready(function(){
                     displayOptions()
                 }
 
+            } else if(snap.val()===3){
+               return;
             }
         }    
     })
-
-  
 
     playersRef.on("value",function(snap){
         numPlayers = snap.numChildren();
@@ -141,8 +151,6 @@ $(document).ready(function(){
         console.log("both ready") 
         }
     })
-    
-
  
     $(".signInButton").on("click",function(){
         event.preventDefault();
@@ -179,26 +187,26 @@ $(document).ready(function(){
 
   
   }) 
-      // function getResult(){
-    //     if ((p1Guess === "r") && (p2Guess === "s")) {
-    //         p1_win++;
-    //         p2_losses++;
-    //       } else if ((userGuess === "r") && (computerGuess === "p")) {
-    //         p1_losses++;
-    //         p2_wins++;
-    //       } else if ((userGuess === "s") && (computerGuess === "r")) {
-    //         p1_losses++;
-    //         p2_wins++;
-    //       } else if ((userGuess === "s") && (computerGuess === "p")) {
-    //         p1_win++;
-    //         p2_losses++;
-    //       } else if ((userGuess === "p") && (computerGuess === "r")) {
-    //         p1_win++;
-    //         p2_losses++;
-    //       } else if ((userGuess === "p") && (computerGuess === "s")) {
-    //         p1_losses++;
-    //         p2_wins++;
-    //       } else if (userGuess === computerGuess) {
-    //         console.log("tie")
-    //       }
-    // }
+  var getResult = function(){
+        if ((p1_choice === "r") && (p2_choice === "s")) {
+            p1_win++;
+            p2_losses++;
+          } else if ((p1_choice=== "r") && (p2_choice === "p")) {
+            p1_losses++;
+            p2_wins++;
+          } else if ((p1_choice === "s") && (p2_choice === "r")) {
+            p1_losses++;
+            p2_wins++;
+          } else if ((p1_choice === "s") && (p2_choice === "p")) {
+            p1_win++;
+            p2_losses++;
+          } else if ((p1_choice === "p") && (p2_choice === "r")) {
+            p1_win++;
+            p2_losses++;
+          } else if ((p1_choice === "p") && (p2_choice === "s")) {
+            p1_losses++;
+            p2_wins++;
+          } else if (p1_choice === p2_choice) {
+            $(".result").text("Tie Time!")
+          }
+    }
