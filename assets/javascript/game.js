33d4a224b2eac4c67180295f955a4f2f7f4a15fb
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-01-16 01:52:41 -0600

    optimize playerCount,correct restart upon one player leave wq

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,115 +15,40 @@ $(document).ready(function(){
     var playerlist = [];
     var numPlayers = 0;
     var inputUserName;
-    var getPlayerInfo = function(){
-            playersRef.on("value",function(snap){
-            numPlayers = snap.numChildren();
-            if(snap.val()){
-                playerlist = Object.keys(snap.val());
-            };
-        })
-    }
-    var displayOptionsP1 = function(){
-        $(".p1").find(".option").show();
-        $(".p1").find(".rock").text("Rock");
-        $(".p1").find(".paper").text("Paper");
-        $(".p1").find(".scissors").text("Scissors");
-        $(".p2").find(".option").hide();
-    };
-    var displayOptionsP2 = function(){
-        $(".p2").find(".option").show();
-        $(".p2").find(".rock").text("Rock");
-        $(".p2").find(".paper").text("Paper");
-        $(".p2").find(".scissors").text("Scissors");
-        $(".p1").find(".option").hide();
-    };
-
-    var displayNameScoresP1 = function(){
-        $(".p1").find(".score").show();
-        $(".p1").find("h2").text(p1.name);
-        $(".p1").find(".wins").text("Wins: "+ p1.wins);
-        $(".p1").find(".losses").text("Losses: "+ p1.losses);
-    }
-    var displayNameScoresP2 = function(){
-        $(".p2").find(".score").show();
-        $(".p2").find("h2").text(p2.name);
-        $(".p2").find(".wins").text("Wins: "+ p2.wins);
-        $(".p2").find(".losses").text("Losses: "+ p2.losses);
-    }
-
-    var updateNameScoresP1 = function(){
-        playersRef.child("p1").on("value", function(snap){
-            if(snap.val()){
-                p1.name = snap.val().name;
-                p1.wins = snap.val().wins;
-                p1.losses = snap.val().losses;
-                displayNameScoresP1();
-            }
-        });
-    }
-    var updateNameScoresP2 = function(){
-        playersRef.child("p2").on("value", function(snap){
-            if(snap.val()){
-                p2.name = snap.val().name;
-                p2.wins = snap.val().wins;
-                p2.losses = snap.val().losses;
-                displayNameScoresP2();
-            }
-        })
-    }
-    var uponTurn1 = function(){
-        $(".p1").addClass("active");
-        $(".p2").removeClass("active");
-        if(p1.role === "user"){
-            displayOptionsP1();
-            displayTurn();
-        } else {
-            displayWait();
-        }        
-    }
-    var uponTurn2 = function(){
-        $(".p2").addClass("active"); 
-        $(".p1").removeClass("active");
-        if(p2.role === "user"){
-            displayOptionsP2();
-            displayTurn();
-        } else{
-            displayWait();
-        }
-    }
-    var uponTurn3 = function(){
-        judge();
-        storeWinsLosses();
-        var timeoutID = setTimeout(startNewRound, 2000)
-    }
-    var uponTurn0 = function(){
-        $(".picked").text("");
-        $(".option").hide();
-        $(".player").removeClass("active");
-        $(".note2").empty();
-    }
-    var uponTurnChange = function(){
-        database.ref().child("turn").on("value",function(snap){
-            if(snap.val()){
-                if(snap.val()===1){
-                    uponTurn1();   
-                } else if(snap.val()===2){
-                    uponTurn2();
-                } else if(snap.val()===3){           
-                    uponTurn3();
-                } else if(snap.val()===0){
-                    uponTurn0();
+    var updateMsg = function(){
+        database.ref("/messages").on("value",function(snap){
+            var storedMessages = snap.val();
+            $("#messages").empty();
+            if(storedMessages == null) {
+                return;
+            } else{
+                var keys = Object.keys(storedMessages);
+                for(var i=0; i<keys.length; i++){
+                    var key = keys[i];
+                    var msgObj = storedMessages[key]
+                    var newMsg = msgObj.text.trim();
+                    var sender = msgObj.user.trim();
+                    var color = sender === inputUserName ? "purple": "";
+                    $("#messages").append(`<p><b style="color: ${color}">${sender}: ${newMsg}<b></p>`);
+                    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+                    console.log($("#messages")[0].scrollHeight)
                 }
-            }    
-        })
+                
+            }
+        }) 
     }
-    var storeDisplayChoice = function(player, choice){
-        playersRef.child(player).update({
-            choice: choice
-        })
-        $(".option").hide();       
-        $("."+ player).find(".picked").text(choice)
-    }  
+    var saveMsg = function(){
+        var msg = $("#msgInput").val();
+        if(inputUserName){
+            database.ref("/messages").push({
+                user: inputUserName,
+                text: msg 
+            })
+        } else{
+            alert("please sign in first");
+        };
+        database.ref("/messages").onDisconnect().remove();
+    }
     var getChoiceP1 = function(){
         playersRef.child("p1").child("choice").on("value",function(snap){
             p1.choice = snap.val()
@@ -134,7 +59,6 @@ $(document).ready(function(){
             p2.choice = snap.val()
         })
     }
-
     var uponP1win = function(){
         p1.wins++;
         p2.losses++;
@@ -172,14 +96,6 @@ $(document).ready(function(){
             losses: p2.losses
         })
     };
-    var startNewRound = function(){
-        $(".picked").text("");
-        $(".result").text("");
-        if(p1.role === "user"){
-            $(".option").show();
-        };
-        database.ref().child("turn").set(1);  
-    }
     var displayWait = function(){
         var name = p1.role === "user" ? p2.name:p1.name;
         var inform = $("<h3>").text("Waiting for " +name+ " to choose")
@@ -189,20 +105,136 @@ $(document).ready(function(){
         var notification = $("<h3>").text("It's your turn!")
         $(".note2").html(notification);
     }
+    var displayOptionsP1 = function(){
+        $(".p1").find(".option").show();
+        $(".p1").find(".rock").text("Rock");
+        $(".p1").find(".paper").text("Paper");
+        $(".p1").find(".scissors").text("Scissors");
+        $(".p2").find(".option").hide();
+    };
+    var displayOptionsP2 = function(){
+        $(".p2").find(".option").show();
+        $(".p2").find(".rock").text("Rock");
+        $(".p2").find(".paper").text("Paper");
+        $(".p2").find(".scissors").text("Scissors");
+        $(".p1").find(".option").hide();
+    };
+    var displayNameScoresP1 = function(){
+        $(".p1").find(".score").show();
+        $(".p1").find("h2").text(p1.name);
+        $(".p1").find(".wins").text("Wins: "+ p1.wins);
+        $(".p1").find(".losses").text("Losses: "+ p1.losses);
+    }
+    var displayNameScoresP2 = function(){
+        $(".p2").find(".score").show();
+        $(".p2").find("h2").text(p2.name);
+        $(".p2").find(".wins").text("Wins: "+ p2.wins);
+        $(".p2").find(".losses").text("Losses: "+ p2.losses);
+    }
+    var updateNameScoresP1 = function(){
+        playersRef.child("p1").on("value", function(snap){
+            if(snap.val()){
+                p1.name = snap.val().name;
+                p1.wins = snap.val().wins;
+                p1.losses = snap.val().losses;
+                displayNameScoresP1();
+            }
+        });
+    }
+    var updateNameScoresP2 = function(){
+        playersRef.child("p2").on("value", function(snap){
+            if(snap.val()){
+                p2.name = snap.val().name;
+                p2.wins = snap.val().wins;
+                p2.losses = snap.val().losses;
+                displayNameScoresP2();
+            }
+        })
+    }
+    var uponTurn1 = function(){
+        $(".p1").addClass("active");
+        $(".p2").removeClass("active");
+        if(p1.role === "user"){
+            displayOptionsP1();
+            displayTurn();
+        } else {
+            displayWait();
+        }        
+    } 
+    var uponTurn2 = function(){
+        $(".p2").addClass("active"); 
+        $(".p1").removeClass("active");
+        if(p2.role === "user"){
+            displayOptionsP2();
+            displayTurn();
+        } else{
+            displayWait();
+        }
+    }
+    var uponTurn3 = function(){
+        if(p1.role === "user"){
+            $(".p2").find(".picked").text(p2.choice)
+            $(".p2").find(".picked").show();
+        } else{
+            $(".p1").find(".picked").text(p1.choice);
+            $(".p1").find(".picked").show();
+        }   
+        judge();
+        storeWinsLosses();         
+        var timeoutID = setTimeout(function(){
+            database.ref().child("turn").set(1);
+            $(".picked").text("");
+        }, 3000)
+    }
+    var uponTurn0 = function(){
+        database.ref().child("messages").set(null);
+        $(".picked").text("");
+        $(".option").hide();
+        $(".player").removeClass("active");
+        $(".note2").empty();
+        $(".result").empty();
+    }
+    var uponTurnChange = function(){
+        database.ref().child("turn").on("value",function(snap){         
+            if(snap.val()){
+                if(snap.val()===1){
+                    uponTurn1();   
+                } else if(snap.val()===2){
+                    uponTurn2();
+                } else if(snap.val()===3){           
+                    uponTurn3();
+                } 
+            } else{uponTurn0();}                      
+        })
+    }
+    var storeDisplayChoice = function(player, choice){
+        playersRef.child(player).update({
+            choice: choice
+        })
+        $("."+ player).find(".option").hide();    
+        $("."+ player).find(".picked").text(choice)
+    } 
+
+    var getPlayerInfo = function(){
+        playersRef.on("value",function(snap){
+        numPlayers = snap.numChildren();
+        if(snap.val()){
+            playerlist = Object.keys(snap.val());
+        };
+    })
+}
     var startGame = function(){
         playersRef.on("child_added",function(snap,prevChildkey){
-            if(prevChildkey){           
-                database.ref().child("turn").set(1);
-                database.ref().child("turn").onDisconnect().remove();  
-                if(p1.role === "user"){
-                    displayOptionsP1();
-                }
+            if(numPlayers===1){           
+                database.ref().child("turn").set(1); 
                 console.log("both ready")
             } 
         })
     }
     var onPlayerLeave = function(){
         playersRef.on("child_removed", function(snap){
+            numPlayers--;
+            database.ref().child("turn").set(0); 
             uponTurn0();
             if(snap.val()){
                 if(p1.role === "user"){
@@ -237,60 +269,6 @@ $(document).ready(function(){
             $(".p2").find("h3").text(userName);
             playersRef.child("p2").onDisconnect().remove() 
     }
-    $(".signInButton").on("click",function(){
-        event.preventDefault();
-        inputUserName = $(".name").val();
-        if(numPlayers === 2){
-            alert("please wait");
-        } else if(numPlayers===1 && playerlist[0]==="p1"){
-            intiateUserP2(inputUserName);
-        } else{
-            intiateUserP1(inputUserName);
-        }        
-        $(".signIn").hide();        
-    });
-    var updateMsg = function(){
-        database.ref("/messages").on("value",function(snap){
-            var storedMessages = snap.val();
-            if(storedMessages == null) {
-                return;
-            } else{
-                $("#messages").empty();
-                var keys = Object.keys(storedMessages);
-                for(var i=0; i<keys.length; i++){
-                    var key = keys[i];
-                    var msgObj = storedMessages[key]
-                    var newMsg = msgObj.text.trim();
-                    var sender = msgObj.user.trim();
-                    var color = sender === inputUserName ? "purple": "";
-                    $("#messages").append(`<p><b style="color: ${color}">${sender}: ${newMsg}<b></p>`);
-                    $("#messages").scrollTop($("#messages")[0].scrollHeight);
-                    console.log($("#messages")[0].scrollHeight)
-                }
-                
-            }
-        })
-    }
-    var saveMsg = function(){
-        var msg = $("#msgInput").val();
-        if(inputUserName){
-            database.ref("/messages").push({
-                user: inputUserName,
-                text: msg 
-            })
-        } else{
-            alert("please sign in first");
-        };
-    }
-    $("#sendBtn").on("click",function(){
-        saveMsg();
-        $("#msgInput").val("");
-    });
-    $("#msgInput").keyup(function(e){
-        if(e.keyCode === 13){
-            $("#sendBtn").click();
-        }
-    });
 
     $(".p1").on("click",".option",function(){
         var c = $(this).attr("data-choice");
@@ -303,6 +281,30 @@ $(document).ready(function(){
         storeDisplayChoice("p2", c);
         database.ref().child("turn").set(3);
     })
+
+    $(".signInButton").on("click",function(){
+        event.preventDefault();
+        inputUserName = $(".name").val();
+        if(numPlayers === 2){
+            alert("please wait");
+        } else if(numPlayers===1 && playerlist[0]==="p1"){
+            intiateUserP2(inputUserName);
+        } else{
+            intiateUserP1(inputUserName);
+        }        
+        $(".signIn").hide();        
+    });
+
+    $("#sendBtn").on("click",function(){
+        saveMsg();
+        $("#msgInput").val("");
+    });
+
+    $("#msgInput").keyup(function(e){
+        if(e.keyCode === 13){
+            $("#sendBtn").click();
+        }
+    });
 
     getPlayerInfo();
     updateNameScoresP1();
